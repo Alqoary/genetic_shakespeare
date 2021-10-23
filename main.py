5b36d2e98563a7d6ab2c7770e53454716a38c8c9
apo apo <alquaaryabdalrhman@gmail.com>
2021-10-23 12:58:52 +0300

    adding genetic algorithm

---
+++ main.py
@@ -0,0 +1,32 @@
+'''
+/**************************************************************
+ *                                                            *
+ *                     Done By: Alqoary                       *
+ *            Github: https://github.com/Alqoary              *
+ *                    Date: 23/10/2021                        *
+ *                                                            * 
+ *************************************************************/
+'''
+from dna import DNA
+from population import Population
+
+
+target = "genetic shakespeare problem"
+pop_size = 450
+mu_ratio = 0.008
+
+
+if __name__ == "__main__":
+    
+    
+    population = Population(target, pop_size, mu_ratio)
+
+    while(1):
+
+        population.selection()
+        population.generation()
+        population.calculate_fitness()
+        population.get_best_score()
+
+        if(population.is_finished):
+            break
